8008c0559d696c7714ee5fcee9b7d4bf81da291b
JekSkeez <98408944+JekSkeez@users.noreply.github.com>
2024-12-23 21:47:13 +0700

    Update test.py

--- test.py
+++ test.py
@@ -255,7 +255,7 @@ async def get_user(update: Update, context: CallbackContext):
             hidden_cookies = f"```json\n{cookies}```"  # Форматируем как код JSON
 
             # Добавляем цитату с куками
-            response += f"> {hidden_cookies.replace('\n', '\n> ')}"
+            response += f"> {hidden_cookies.replace('\n', '\\n> ')}"  # Заменяем \n на \n> для цитаты
 
             await send_message(update, response)
             return
